22b1ca8c1e816789bd5f7b6b212095ae91f10a48
irf-rox <94896261+irf-rox@users.noreply.github.com>
2023-03-22 18:08:56 +0530

    Added auto pip download feature

--- rfid.py
+++ rfid.py
@@ -1,134 +1,177 @@
-from tkinter import *
-import customtkinter
-from PIL import ImageTk,Image
-from tkinter import ttk
-from tkinter import messagebox
-from functools import partial
-import mysql.connector as sql
-import os
-from tkinter.filedialog import askopenfilename
-import csv
+try:
+    import tkinter
+    import tkinter.messagebox
+    import customtkinter
+    import serial
+    from pyfirmata import Arduino, util
+except:
+    import pip
+    pip.main(['install','pyfirmata'])
+    from pyfirmata import Arduino, util
+    pip.main(['install','customtkinter'])
+    import customtkinter
+    pip.main(['install','pyserial'])
+    import serial
 
-def sup(*funcs):
-    def combined_func(*args,**kwargs):
-        for f in funcs:
-            f(*args,**kwargs)
-    return combined_func
+ser = serial.Serial('COM4', 9600)
 
-customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
-root = customtkinter.CTk()
-root.geometry('1280x680')
-root.resizable(width='False',height='False')
-root.title("Vehicle Details Detector Pro")
 
-def f8():
-    frame_8 = customtkinter.CTkFrame(master=root)
-    frame_8.pack(pady=20, padx=60, fill="both", expand=True)
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
 
-    button_1 = customtkinter.CTkButton(master=frame_8, text="Done",command=sup(frame_8.destroy,f3))
-    button_1.place(relx=0.45, rely=0.8)
+        # configure window
+        self.title("CustomTkinter complex_example.py")
+        self.geometry(f"{1100}x{580}")
 
-def f7():
-    frame_7 = customtkinter.CTkFrame(master=root)
-    frame_7.pack(pady=20, padx=60, fill="both", expand=True)
+        # configure grid layout (4x4)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
 
-    label_5 = customtkinter.CTkLabel(master=frame_7, justify=customtkinter.LEFT,text="Tap your RFID tag now",font=("Book Antiqua",50))
-    label_5.place(relx=0.15,rely=0.3)
+        # create sidebar frame with widgets
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(4, weight=1)
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="CustomTkinter", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
+        self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
+        self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
+        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
+        self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
+        self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
+        self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
+                                                                       command=self.change_appearance_mode_event)
+        self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
+        self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
+        self.scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
+        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
+                                                               command=self.change_scaling_event)
+        self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
 
-    button_1 = customtkinter.CTkButton(master=frame_7, text="Back",command=sup(frame_7.destroy,f3))
-    button_1.place(relx=0.86, rely=0.04)
+        # create main entry and button
+        self.entry = customtkinter.CTkEntry(self, placeholder_text="CTkEntry")
+        self.entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
 
-def f6():
-    frame_6 = customtkinter.CTkFrame(master=root)
-    frame_6.pack(pady=20, padx=60, fill="both", expand=True)
+        self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"))
+        self.main_button_1.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
-    label_1 = customtkinter.CTkLabel(master=frame_6, justify=customtkinter.LEFT,text="Name :")
-    label_1.place(rely=0.3, relx=0.09)
+        # create textbox
+        self.textbox = customtkinter.CTkTextbox(self, width=250)
+        self.textbox.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
 
-    entry_1 = customtkinter.CTkEntry(master=frame_6, width=250)
-    entry_1.place(rely=0.3, relx=0.25)
+        # create tabview
+        self.tabview = customtkinter.CTkTabview(self, width=250)
+        self.tabview.grid(row=0, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.tabview.add("CTkTabview")
+        self.tabview.add("Tab 2")
+        self.tabview.add("Tab 3")
+        self.tabview.tab("CTkTabview").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
+        self.tabview.tab("Tab 2").grid_columnconfigure(0, weight=1)
 
-    label_2 = customtkinter.CTkLabel(master=frame_6, justify=customtkinter.LEFT, text="Phone number :")
-    label_2.place(rely=0.4, relx=0.09)
+        self.optionmenu_1 = customtkinter.CTkOptionMenu(self.tabview.tab("CTkTabview"), dynamic_resizing=False,
+                                                        values=["Value 1", "Value 2", "Value Long Long Long"])
+        self.optionmenu_1.grid(row=0, column=0, padx=20, pady=(20, 10))
+        self.combobox_1 = customtkinter.CTkComboBox(self.tabview.tab("CTkTabview"),
+                                                    values=["Value 1", "Value 2", "Value Long....."])
+        self.combobox_1.grid(row=1, column=0, padx=20, pady=(10, 10))
+        self.string_input_button = customtkinter.CTkButton(self.tabview.tab("CTkTabview"), text="Open CTkInputDialog",
+                                                           command=self.open_input_dialog_event)
+        self.string_input_button.grid(row=2, column=0, padx=20, pady=(10, 10))
+        self.label_tab_2 = customtkinter.CTkLabel(self.tabview.tab("Tab 2"), text="CTkLabel on Tab 2")
+        self.label_tab_2.grid(row=0, column=0, padx=20, pady=20)
 
-    entry_2 = customtkinter.CTkEntry(master=frame_6, width=250)
-    entry_2.place(rely=0.4, relx=0.25)
+        # create radiobutton frame
+        self.radiobutton_frame = customtkinter.CTkFrame(self)
+        self.radiobutton_frame.grid(row=0, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        self.radio_var = tkinter.IntVar(value=0)
+        self.label_radio_group = customtkinter.CTkLabel(master=self.radiobutton_frame, text="CTkRadioButton Group:")
+        self.label_radio_group.grid(row=0, column=2, columnspan=1, padx=10, pady=10, sticky="")
+        self.radio_button_1 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=0)
+        self.radio_button_1.grid(row=1, column=2, pady=10, padx=20, sticky="n")
+        self.radio_button_2 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=1)
+        self.radio_button_2.grid(row=2, column=2, pady=10, padx=20, sticky="n")
+        self.radio_button_3 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=2)
+        self.radio_button_3.grid(row=3, column=2, pady=10, padx=20, sticky="n")
 
-    label_3 = customtkinter.CTkLabel(master=frame_6, justify=customtkinter.LEFT, text="License number :")
-    label_3.place(rely=0.5, relx=0.09)
+        # create slider and progressbar frame
+        self.slider_progressbar_frame = customtkinter.CTkFrame(self, fg_color="transparent")
+        self.slider_progressbar_frame.grid(row=1, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.slider_progressbar_frame.grid_columnconfigure(0, weight=1)
+        self.slider_progressbar_frame.grid_rowconfigure(4, weight=1)
+        self.seg_button_1 = customtkinter.CTkSegmentedButton(self.slider_progressbar_frame)
+        self.seg_button_1.grid(row=0, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+        self.progressbar_1 = customtkinter.CTkProgressBar(self.slider_progressbar_frame)
+        self.progressbar_1.grid(row=1, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+        self.progressbar_2 = customtkinter.CTkProgressBar(self.slider_progressbar_frame)
+        self.progressbar_2.grid(row=2, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+        self.slider_1 = customtkinter.CTkSlider(self.slider_progressbar_frame, from_=0, to=1, number_of_steps=4)
+        self.slider_1.grid(row=3, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+        self.slider_2 = customtkinter.CTkSlider(self.slider_progressbar_frame, orientation="vertical")
+        self.slider_2.grid(row=0, column=1, rowspan=5, padx=(10, 10), pady=(10, 10), sticky="ns")
+        self.progressbar_3 = customtkinter.CTkProgressBar(self.slider_progressbar_frame, orientation="vertical")
+        self.progressbar_3.grid(row=0, column=2, rowspan=5, padx=(10, 20), pady=(10, 10), sticky="ns")
 
-    entry_3 = customtkinter.CTkEntry(master=frame_6, width=250)
-    entry_3.place(rely=0.5, relx=0.25)
+        # create scrollable frame
+        self.scrollable_frame = customtkinter.CTkScrollableFrame(self, label_text="CTkScrollableFrame")
+        self.scrollable_frame.grid(row=1, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.scrollable_frame.grid_columnconfigure(0, weight=1)
+        self.scrollable_frame_switches = []
+        for i in range(100):
+            switch = customtkinter.CTkSwitch(master=self.scrollable_frame, text=f"CTkSwitch {i}")
+            switch.grid(row=i, column=0, padx=10, pady=(0, 20))
+            self.scrollable_frame_switches.append(switch)
 
-    label_4 = customtkinter.CTkLabel(master=frame_6, justify=customtkinter.LEFT, text="Vehicle name :")
-    label_4.place(rely=0.6, relx=0.09)
+        # create checkbox and switch frame
+        self.checkbox_slider_frame = customtkinter.CTkFrame(self)
+        self.checkbox_slider_frame.grid(row=1, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        self.checkbox_1 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
+        self.checkbox_1.grid(row=1, column=0, pady=(20, 0), padx=20, sticky="n")
+        self.checkbox_2 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
+        self.checkbox_2.grid(row=2, column=0, pady=(20, 0), padx=20, sticky="n")
+        self.checkbox_3 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
+        self.checkbox_3.grid(row=3, column=0, pady=20, padx=20, sticky="n")
 
-    entry_4 = customtkinter.CTkEntry(master=frame_6, width=250)
-    entry_4.place(rely=0.6, relx=0.25)
+        # set default values
+        self.sidebar_button_3.configure(state="disabled", text="Disabled CTkButton")
+        self.checkbox_3.configure(state="disabled")
+        self.checkbox_1.select()
+        self.scrollable_frame_switches[0].select()
+        self.scrollable_frame_switches[4].select()
+        self.radio_button_3.configure(state="disabled")
+        self.appearance_mode_optionemenu.set("Dark")
+        self.scaling_optionemenu.set("100%")
+        self.optionmenu_1.set("CTkOptionmenu")
+        self.combobox_1.set("CTkComboBox")
+        self.slider_1.configure(command=self.progressbar_2.set)
+        self.slider_2.configure(command=self.progressbar_3.set)
+        self.progressbar_1.configure(mode="indeterminnate")
+        self.progressbar_1.start()
+        self.textbox.insert("0.0", "CTkTextbox\n\n" + "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.\n\n" * 20)
+        self.seg_button_1.configure(values=["CTkSegmentedButton", "Value 2", "Value 3"])
+        self.seg_button_1.set("Value 2")
 
-    button_5 = customtkinter.CTkButton(master=frame_6, command=sup(frame_6.destroy,f3),text="Back")
-    button_5.place(relx=0.85,rely=0.05)
+    def open_input_dialog_event(self):
+        dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
+        print("CTkInputDialog:", dialog.get_input())
 
-    button_1 = customtkinter.CTkButton(master=frame_6, text="Submit",command=sup(frame_6.destroy,f7))
-    button_1.place(rely=0.8, relx=0.4)
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        customtkinter.set_appearance_mode(new_appearance_mode)
 
-def f5():
-    frame_5 = customtkinter.CTkFrame(master=root)
-    frame_5.pack(pady=20, padx=60, fill="both", expand=True)
+    def change_scaling_event(self, new_scaling: str):
+        new_scaling_float = int(new_scaling.replace("%", "")) / 100
+        customtkinter.set_widget_scaling(new_scaling_float)
 
-    label_6 = customtkinter.CTkLabel(master=frame_5, justify=customtkinter.LEFT, text="Scan Successful",font=("Book Antiqua", 50))
-    label_6.place(relx=0.25, rely=0.2)
+    def sidebar_button_event(self):
+        print("sidebar_button click")
 
-    label_6 = customtkinter.CTkLabel(master=frame_5, justify=customtkinter.LEFT, text="Details:",font=("Book Antiqua", 50))
-    label_6.place(relx=0.25, rely=0.35)
 
-    button_5 = customtkinter.CTkButton(master=frame_5, text="FINISH", command=sup(frame_5.destroy,f3))
-    button_5.place(relx=0.45, rely=0.85)
-
-def f4():
-    frame_4 = customtkinter.CTkFrame(master= root)
-    frame_4.pack(pady=20, padx=60, fill="both", expand=True)
-
-    label_5 = customtkinter.CTkLabel(master=frame_4, justify=customtkinter.LEFT,text="Scanning for Vehicle",font=("Book Antiqua",50))
-    label_5.place(relx=0.15,rely=0.3)
-
-    button_5 = customtkinter.CTkButton(master=frame_4, command=sup(frame_4.destroy,f3),text="Back")
-    button_5.place(relx=0.85,rely=0.05)
-
-def f3():
-    frame_3 = customtkinter.CTkFrame(master=root)
-    frame_3.pack(pady=20, padx=60, fill="both", expand=True)
-
-    button_1 = customtkinter.CTkButton(master=frame_3, text= "Scan Tag", font=("book antiqua",30), command=sup(frame_3.destroy,f4))
-    button_1.place(relx=0.6, rely= 0.45)
-
-    button_2 = customtkinter.CTkButton(master=frame_3, text= "Load Tag", font=("book antiqua",30), command=sup(frame_3.destroy,f6))
-    button_2.place(relx=0.3, rely= 0.45)
-
-    button = customtkinter.CTkButton(master=frame_3, text="Log-Out", command=sup(frame_3.destroy,f1))
-    button.place(relx=0.47, rely=0.8)
-
-def f1():
-    frame_1 = customtkinter.CTkFrame(master=root)
-    frame_1.pack(pady=30, padx=60, fill="both", expand=True)
-
-    label_1 = customtkinter.CTkLabel(master=frame_1, justify=customtkinter.LEFT,text="USER LOGIN",font=("book antiqua",50))
-    label_1.place(relx=0.35, rely=0.1)
-   
-    button = customtkinter.CTkButton(master=frame_1, text="LOGIN", command=sup(frame_1.destroy,f3))
-    button.place(relx=0.35, rely=0.58)
-
-    entry_1 = customtkinter.CTkEntry(master=frame_1, placeholder_text="ID",width=200)
-    entry_1.place(relx=0.38,rely=0.33)
-
-    entry_1 = customtkinter.CTkEntry(master=frame_1, placeholder_text="PASSWORD", show = "•", width=200)
-    entry_1.place(relx=0.38,rely=0.44)
-
-    button = customtkinter.CTkButton(master=frame_1, text="Sign-Up here", command=sup(frame_1.destroy,f3))
-    button.place(relx=0.5, rely=0.58)
-
-f1() #function call
-root.mainloop()
-#End of code
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
